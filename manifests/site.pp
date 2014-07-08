dff1fd7f3681e5f6c48a1e7d7572efff44f3c87e
Douglas Nomizo <douglasnomizo@outlook.com>
2014-07-08 09:01:18 -0300

    Douglas - Added basic configuration and software

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,6 +52,34 @@ Service {
 Homebrew::Formula <| |> -> Package <| |>
 
 node default {
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::tap_to_click
+  class { 'osx::global::key_repeat_rate': rate => 0.6 }
+  class { 'osx::global::key_repeat_delay': delay => 1.5 }
+  include osx::dock::autohide
+  include osx::dock::icon_size
+  include osx::finder::show_all_on_desktop
+  include osx::finder::empty_trash_securely
+  include osx::finder::unhide_library
+  include osx::software_update
+  include osx::keyboard::capslock_to_control
+  include osx::no_network_dsstores
+
+  class { 'osx::dock::hot_corners':
+    bottom_left => "Put Display to Sleep",
+  }
+
+  git::config::global { 'user.email':
+    value  => 'douglasnomizo@outlook.com'
+  }
+
+  git::config::global { 'user.name':
+    value  => 'Douglas Nomizo'
+  }
+
+  
   # core modules, needed for most things
   include dnsmasq
   include git
@@ -67,6 +95,11 @@ node default {
   include nodejs::v0_6
   include nodejs::v0_8
   include nodejs::v0_10
+ 
+  nodejs::module { 'coffee-script':
+    node_version => 'v0.10',
+    ensure => '1.6.3',
+  }
 
   # default ruby versions
   ruby::version { '1.9.3': }
@@ -74,18 +107,67 @@ node default {
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
+  
+  class { 'ruby::global':
+    version => '2.1.2'
+  }
+
 
   # common, useful packages
   package {
     [
       'ack',
       'findutils',
-      'gnu-tar'
+      'gnu-tar',
+      'nmap'
     ]:
   }
 
-  file { "${boxen::config::srcdir}/our-boxen":
+  file { "${boxen::config::srcdir}/myboxen":
     ensure => link,
     target => $boxen::config::repodir
   }
+  
+  include java
+  include python
+
+  include chrome
+  include chrome::dev
+  include firefox
+
+  include vagrant
+  include virtualbox
+  include heroku
+  class { 'intellij':
+    edition => 'ultimate',
+    version => '13'
+  }
+
+  include postgresql
+  include mysql
+  include mongodb
+
+  include sublime_text_2
+
+  include alfred
+  include caffeine
+  include sizeup
+  include onepassword
+  include keyremap4macbook
+  include zsh
+  include screen
+  include wget
+  include autojump
+  include ctags
+  include tmux
+
+  include steam
+  include evernote
+  include omnigraffle::pro
+  include dropbox
+  include googledrive
+  include skype
+  include adium
+  include vlc
+  include hipchat
 }
